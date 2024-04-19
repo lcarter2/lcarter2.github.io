32beba6f6543b98f488b71a65e43c100dfc3d6b3
lcarter2 <lcarter@arka.org>
2024-04-19 09:40:29 -0400

    Testing Python scripts

---
+++ MYSCRIPT.py
@@ -0,0 +1,28 @@
+import os
+from openpyxl import load_workbook
+
+def update_files(directory):
+
+    os.chdir(directory)
+    directory_completed = os.fsencode(directory)
+    for file in os.listdir(directory_completed):
+        filename = os.fsdecode(file)
+        if '.xlsx' in filename and filename[:2] != '~$': #Avoid temporary files
+            wb = load_workbook(filename)
+            ws = wb['Form']
+            
+            old_num = ws['C5'].value
+            new_num = filename.split('_')[0]
+            
+            if int(old_num) != int(new_num):
+                print(old_num, " -> ", new_num)
+            
+            ws.cell(row=5, column=3).value = int(new_num)
+            
+            wb.save(filename)
+            wb.close()
+        
+        
+if __name__ == '__main__':
+    update_files('C:\\ForLogan\\MSTestCases\\Foo')
+    print('Done')
